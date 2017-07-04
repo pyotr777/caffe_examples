6656909c73424a5609d2661daa6c6388645e0eb2
Peter Bryzgalov <peterbryz@yahoo.com>
2017-07-04 18:16:16 +0900

    Look close to Keras MNIST.

--- mnist/train_lenet.sh
+++ mnist/train_lenet.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-$CAFFE_ROOT/build/tools/caffe train --solver=./lenet_solver.prototxt $@
+./build/tools/caffe train --solver=examples/mnist/lenet_solver.prototxt $@
--- sync_down_tegra.sh
+++ sync_down_tegra.sh
@@ -2,4 +2,4 @@
 
 echo "rsync from remote Tegra-ubuntu-1:caffe/examples"
 set -x
-rsync -avc $@ --exclude=".*" --include="*.test" --include="*.train" --include="*plot*" --include="*.png" --include="*.ipynb" --include="mnist" --include="mnist/*.prototxt" --exclude="*" tegra-ubuntu-1:caffe/examples/ .
+rsync -avc $@ --exclude=".*" --include="*.sh" --include="*.test" --include="*.train" --include="*plot*" --include="*.png" --include="*.ipynb" --include="mnist" --include="mnist/*.prototxt" --exclude="*" tegra-ubuntu-1:caffe/examples/ .
