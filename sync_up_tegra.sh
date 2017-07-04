1d98f24897d5a7931c2839450826f4073997d52a
Peter Bryzgalov <peterbryz@yahoo.com>
2017-07-04 14:50:30 +0900

    Run coffe in CLI and plot graph with plotly.

--- sync_up_tegra.sh
+++ sync_up_tegra.sh
@@ -2,4 +2,4 @@
 
 echo "rsync from local to Tegra-ubuntu-1:caffe/examples"
 set -x
-rsync -avc $@ --include="*.ipynb" --include="mnist" --include="mnist/*.prototxt" --exclude="*" . tegra-ubuntu-1:caffe/examples/
+rsync -avc $@ --exclude=".*" --include="*plot*" --include="*.ipynb" --include="mnist" --include="mnist/*.prototxt" --exclude="*" . tegra-ubuntu-1:caffe/examples/
