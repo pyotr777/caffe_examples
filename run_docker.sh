b216b8e6ef41fadac46ea424e9df37a5ab47b16a
Peter Bryzgalov <peterbryz@yahoo.com>
2017-07-19 20:10:54 +0900

    Install Jupyter in Docker image. Start Jupyter in container.

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti --name caffe -v $(pwd):/root/ pyotr777/caffe
+docker run -ti --name caffe -v $(pwd):/root/ -p 8888:8888 pyotr777/caffe-jupyter jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token=""
